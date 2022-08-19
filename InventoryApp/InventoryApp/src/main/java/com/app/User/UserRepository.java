7933b8633ed1b2f0a228f5877a6d2ce078e76ba6
Kartikey <kartikey.saran93@gmail.com>
2022-08-20 00:17:41 +0530

    deleted all

--- InventoryApp/InventoryApp/src/main/java/com/app/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.User;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByNameAndPassword(String name, String password );
-}
